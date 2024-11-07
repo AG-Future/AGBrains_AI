98803e8e188b7273e6e8eed20df6bea8c331dd68
komojo16 <komojo16@gmail.com>
2024-11-07 21:31:26 +0900

    max hand optimize

--- hand_main.py
+++ hand_main.py
@@ -1,28 +1,63 @@
 from typing import Tuple
-import math
 import cv2
 import mediapipe as mp
 from tensorflow.python.ops.signal.shape_ops import frame
 
 from send_request import send_request
 
+send_request_true = 0
+hands_num = 1
+camera_set = 0
+
+
 try:
     print("Input stayLength(float number): ", end='')
     stayLength = float(input())
+
+
 except ValueError:
     stayLength = 5000
     print(f"only Numbers are allowed, now stayLength is default({stayLength})")
+#---------------------------------------
+print("send_request(y/n): ", end='')
+temp = input()
+
+if temp == 'y' or temp == 'Y':
+    send_request_true = 1
+if temp not in ['y', 'Y', 'n', 'N']:
+    print("Please enter y or N, now hands is default(n)")
+#---------------------------------------
+try:
+    print("max_num_hands(1 or 2): ", end='')
+    hands_num = int(input())
+    if hands_num in [1, 2]:
+        pass
+    else:
+        print("Please enter 1 or 2, now hands is default(1)")
+except ValueError:
+    print("Please enter a number, now hands is default(1)")
+
+
+try:
+    print("camera set(0 or 2): ", end='')
+    camera_set = int(input())
+    if camera_set in [0, 2]:
+        pass
+    else:
+        print("Please enter 0 or 2, now camera is default(0)")
+except ValueError:
+    print("Please enter a number, now camera is default(0)")
+
+
 
 # 미디어 파이프의 Hand 모델을 로드합니다.
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-
 HAND_SIZE_THRESHOLD = 3200
 HAND_SIZE_MINI = 2700
 
-
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(camera_set)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1250)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
@@ -35,7 +70,7 @@ def calculate_hand_size():
 
     # 유클리드 거리 계산
     hand_size = length_of((middle.x, middle.y), (wrist.x, wrist.y))
-    print(hand_size)
+    #print(hand_size)
 
     text = "****WARNING: The distance is too far to be recognized.***"
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -48,7 +83,7 @@ def calculate_hand_size():
     text_y = (height + text_size[1]) // 2  # 프레임의 세로 중앙에 텍스트의 세로 중심 맞추기
 
     hand_z = abs(abs(hand_landmarks.landmark[9].z) - abs(hand_landmarks.landmark[0].z))
-    #print(f'hand_size: {hand_size}\nhand_z: {hand_z}\nhand9z: {hand_landmarks.landmark[9]}\nhand0z: {hand_landmarks.landmark[0].z}')
+    # print(f'hand_size: {hand_size}\nhand_z: {hand_z}\nhand9z: {hand_landmarks.landmark[9]}\nhand0z: {hand_landmarks.landmark[0].z}')
     if hand_size < HAND_SIZE_MINI:
         cv2.putText(frame, text, (text_x, text_y), font, font_scale, color, thickness, cv2.LINE_AA)
 
@@ -63,8 +98,8 @@ def calculate_hand_size():
     hand9z: x: 0.464547366
     y: 0.69831568
     z: 0.00071023294
-    
-    
+
+
     hand0z: 4.253604402038036e-08
     hand_size: 0.01819072473518678
     hand_z: 0.021593765719003954
@@ -72,8 +107,6 @@ def calculate_hand_size():
     y: 0.627887487
     z: -0.0215938296
     '''
-
-
 
 
 def get_direction(p1: Tuple[float, float], p2: Tuple[float, float]):
@@ -92,13 +125,16 @@ def get_direction(p1: Tuple[float, float], p2: Tuple[float, float]):
         else:
             return "left"
 
+
 def get_actual_position(point: Tuple[float, float]) -> Tuple[float, float]:
     return point[0] * cap.get(cv2.CAP_PROP_FRAME_WIDTH), point[1] * cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
 
 def length_of(p1: Tuple[float, float], p2: Tuple[float, float]):
     p1 = get_actual_position(p1)
     p2 = get_actual_position(p2)
     return (p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2
+
 
 def get_tilt(p1: Tuple[float, float], p2: Tuple[float, float]):
     p1 = get_actual_position(p1)
@@ -107,14 +143,15 @@ def get_tilt(p1: Tuple[float, float], p2: Tuple[float, float]):
         return 1j
     return (p2[1] - p1[1]) / (p2[0] - p1[0])
 
+
 def x_y():
     x1 = hand_landmarks.landmark[12].x
     y1 = hand_landmarks.landmark[12].y
-    #z1 = (hand_landmarks.landmark[12].z * 100) // 1
+    # z1 = (hand_landmarks.landmark[12].z * 100) // 1
 
     x2 = hand_landmarks.landmark[0].x
     y2 = hand_landmarks.landmark[0].y
-    #z2 = (hand_landmarks.landmark[0].z * 100) // 1
+    # z2 = (hand_landmarks.landmark[0].z * 100) // 1
 
     direction = get_direction((x1, y1), (x2, y2))
     '''
@@ -142,7 +179,7 @@ def x_y():
         thickness=2
     )
     '''
-    send_request(direction)
+    if send_request_true: send_request(direction)
 
     cv2.putText(
         frame,
@@ -157,11 +194,7 @@ def x_y():
     )
 
 
-
-
-
-
-with mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+with mp_hands.Hands(max_num_hands=hands_num,min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
     while cap.isOpened():
         success, frame = cap.read()  # 변수 이름을 'frame'으로 변경
         if not success:
@@ -180,17 +213,10 @@ with mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.5, min_tracking_
                 calculate_hand_size()
                 x_y()
 
-
-
         cv2.imshow('frame', frame)  # 창 이름도 'frame'으로 변경
 
-
-
-
         if cv2.waitKey(1) == ord('q'):
-
             break
-
 
 cap.release()
 cv2.destroyAllWindows()
@@ -199,4 +225,3 @@ cv2.destroyAllWindows()
 1:-0.010069087147712708
 0:1.236920752489823e-07
 '''
-
